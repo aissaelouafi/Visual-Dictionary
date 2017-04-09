cd437e432985cffa0b3624ac338c4dbf8498482c
Aissa EL OUAFI <aissaelouafi@MacBook-Pro-de-Aissa.local>
2017-04-09 15:47:24 +0200

    Add sub topics description extractor

--- python_scripts/extractTopicDescription.py
+++ python_scripts/extractTopicDescription.py
@@ -1,1 +1,5 @@
-TODO : Loop on json file and get the most long text of every sub-topics pages, this will be the descrption of this sub topics
+import glob,os,json,re
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+json_
